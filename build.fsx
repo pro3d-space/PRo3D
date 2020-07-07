6b694499cc4f28530de8957b743ab50e26220087
Harald Steinlechner <haraldsteinlechner@gmail.com>
2020-07-07 13:09:55 +0200

    fixed native dependency injection (using base now); added script for copying other resources (groupmappings)

--- build.fsx
+++ build.fsx
@@ -7,6 +7,7 @@ open System.IO
 open System.Diagnostics
 open Aardvark.Fake
 open Fake.Core
+open Fake.Core.TargetOperators
 open Fake.Tools
 open Fake.IO.Globbing.Operators
 open System.Runtime.InteropServices
@@ -14,12 +15,37 @@ open System.Runtime.InteropServices
 //do MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some Minimal }
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
+
+open Fake.IO
+
+
+let outDirs = [ @"bin\Debug\netcoreapp3.1"; @"bin\Release\netcoreapp3.1"]
+let resources = 
+    [
+        //"lib\Dependencies\PRo3D.Base\windows"; // currently handled by native dependency injection mechanism 
+        "lib/groupmappings"
+    ]
+
+
+Target.create "CopyResources" (fun _ -> 
+    for r in resources do
+        for outDir in outDirs do
+            if Directory.Exists outDir then
+                if Path.isDirectory r then 
+                    printfn "copying dir %s => %s" r outDir
+                    Shell.copyDir outDir r (fun _ -> true)
+                else 
+                    printfn "copying file %s => %s" r outDir
+                    Shell.copyFile outDir r
+)
+
 DefaultSetup.install ["src/PRo3D.sln"]
 
-//#if DEBUG
+"Compile" ==> "CopyResources" ==> "AddNativeResources" |> ignore
+
+
+#if DEBUG
 do System.Diagnostics.Debugger.Launch() |> ignore
-//#endif
-
-
+#endif
 
 entry()
