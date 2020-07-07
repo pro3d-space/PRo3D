9b2b8915eacabacc0a69db94141a8c0219271f4b
Harald Steinlechner <haraldsteinlechner@gmail.com>
2020-07-07 12:08:16 +0200

    fixed build script, native libs no longer injected

--- build.fsx
+++ build.fsx
@@ -2,7 +2,6 @@
 #load ".fake/build.fsx/intellisense.fsx"
 #load @"paket-files/build/aardvark-platform/aardvark.fake/DefaultSetup.fsx"
 
-open Fake
 open System
 open System.IO
 open System.Diagnostics
@@ -10,15 +9,17 @@ open Aardvark.Fake
 open Fake.Core
 open Fake.Tools
 open Fake.IO.Globbing.Operators
+open System.Runtime.InteropServices
 
 //do MSBuildDefaults <- { MSBuildDefaults with Verbosity = Some Minimal }
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 DefaultSetup.install ["src/PRo3D.sln"]
 
-#if DEBUG
+//#if DEBUG
 do System.Diagnostics.Debugger.Launch() |> ignore
-#endif
+//#endif
+
 
 
 entry()
