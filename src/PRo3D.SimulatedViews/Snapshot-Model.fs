5fa50ee55eaceaeebde0de29f7330af2cae05bc7
Rebecca Nowak <rnowak@vrvis.at>
2020-11-20 10:17:05 +0100

    updated Snapshot-Model.fs, renamed project

--- src/PRo3D.SimulatedViews/Snapshot-Model.fs
+++ src/PRo3D.SimulatedViews/Snapshot-Model.fs
@@ -20,6 +20,19 @@ module Json =
             f a name
         | None ->
             Json.writeNone name
+      let writeOptionFloat (name : string) (x : option<float>) =
+        match x with
+        | Some a ->
+            Json.write name a
+        | None ->
+            Json.writeNone name
+
+      let writeOptionInt (name : string) (x : option<int>) =
+        match x with
+        | Some a ->
+            Json.write name a
+        | None ->
+            Json.writeNone name
 
 type HeraCam = {
     location      : V3d
@@ -89,6 +102,7 @@ type SnapshotCamera = {
 
 type SnapshotSurfaceUpdate = {
     surfname      : string
+    translation   : option<V3d>
     trafo         : option<Trafo3d>
     visible       : option<bool>
 } 
@@ -96,6 +110,7 @@ with
   static member TestData =
     {
         surfname = "testname"
+        translation =  0.0 |> V3d |> Some
         trafo    = Some Trafo3d.Identity
         visible  = Some true
     }
@@ -105,11 +120,13 @@ with
         let! opcname    = Json.read "opcname"
         let! trafo       = Json.parseOption (Json.tryRead "trafo") Trafo3d.Parse
         let! visible     = Json.tryRead "visible"
+        let! translation = Json.parseOption (Json.tryRead "translation") V3d.Parse
         
         let res = {
             surfname = opcname
             trafo    = trafo
             visible  = visible
+            translation = translation
         }
         return res
       }
@@ -122,47 +139,57 @@ with
       do! Json.write        "opcname"  (x.surfname.ToString())
       do! Json.writeOption  "trafo"     x.trafo
       do! Json.write  "visible"   x.visible
+      do! Json.writeOption "translation" x.translation
     }
 
 type SnapshotShattercone = {
-    name        : string
-    count       : int
-    color       : option<C4b>
-    contrast    : option<float>
-    brightness  : option<float>
-    gamma       : option<float>
-    scale       : option<V2i> // [min,max]
-    xRotation   : option<V2i> // [min,max] in degree?
-    yRotation   : option<V2i> 
-    zRotation   : option<V2i>
+    name         : string
+    count        : int
+    color        : option<C4b>
+    contrast     : option<float>
+    brightness   : option<float>
+    gamma        : option<float>
+    scale        : option<V2i> // [min,max]
+    xRotation    : option<V2i> // [min,max] in degree?
+    yRotation    : option<V2i> 
+    zRotation    : option<V2i>
+    maxDistance  : option<float>
+    subsurface   : option<int>
+    maskColor    : option<C4b>
 } 
 with 
   static member TestData =
     {
-        name       = "Steinheim_Germany_Model_1.obj"
-        count      = 10
-        color      = Some C4b.Green //(C4b(242, 198, 153, 255))
-        contrast   = None
-        brightness = None
-        gamma      = None
-        scale      = Some (V2i(1, 5))
-        xRotation  = None
-        yRotation  = Some (V2i(0, 180))
-        zRotation  = Some (V2i(0, 360))
+        name         = "02-Presqu-ile-LF.obj"//"Steinheim_Germany_Model_1.obj"
+        count        = 4
+        color        = None //Some (C4b(242, 198, 153, 255))
+        contrast     = None
+        brightness   = None
+        gamma        = None
+        scale        = Some (V2i(30, 100))
+        xRotation    = None
+        yRotation    = None //Some (V2i(0, 180))
+        zRotation    = Some (V2i(0, 360))
+        maxDistance  = None // Some 5.0
+        subsurface   = Some 50 //None
+        maskColor    = Some C4b.Green
     }
   static member current = 0
   static member private readV0 = 
       json {
-        let! name       = Json.read "name"
-        let! count      = Json.read "count"
-        let! color      = Json.parseOption (Json.tryRead "color") C4b.Parse
-        let! contrast   = Json.tryRead "contrast"
-        let! brightness = Json.tryRead "brightness"
-        let! gamma      = Json.tryRead "gamma"
-        let! scale      = Json.parseOption (Json.tryRead "scale") V2i.Parse 
-        let! xRotation  = Json.parseOption (Json.tryRead "xRotation") V2i.Parse 
-        let! yRotation  = Json.parseOption (Json.tryRead "yRotation") V2i.Parse 
-        let! zRotation  = Json.parseOption (Json.tryRead "zRotation") V2i.Parse 
+        let! name        = Json.read "name"
+        let! count       = Json.read "count"
+        let! color       = Json.parseOption (Json.tryRead "color") C4b.Parse
+        let! contrast    = Json.tryRead "contrast"
+        let! brightness  = Json.tryRead "brightness"
+        let! gamma       = Json.tryRead "gamma"
+        let! scale       = Json.parseOption (Json.tryRead "scale") V2i.Parse 
+        let! xRotation   = Json.parseOption (Json.tryRead "xRotation") V2i.Parse 
+        let! yRotation   = Json.parseOption (Json.tryRead "yRotation") V2i.Parse 
+        let! zRotation   = Json.parseOption (Json.tryRead "zRotation") V2i.Parse 
+        let! maxDistance = Json.tryRead "maxDistance"
+        let! subsurface  = Json.tryRead "subsurface"
+        let! maskColor   = Json.parseOption (Json.tryRead "maskColor") C4b.Parse
         
         let res = {
             name        = name
@@ -175,6 +202,9 @@ with
             xRotation   = xRotation 
             yRotation   = yRotation 
             zRotation   = zRotation   
+            maxDistance  = maxDistance
+            subsurface   = subsurface 
+            maskColor    = maskColor            
         }
         return res
       }
@@ -184,14 +214,22 @@ with
     }
   static member ToJson (x : SnapshotShattercone) =
     json {
-      do! Json.write        "name"          (x.name.ToString())
-      do! Json.write        "count"         x.count
-      do! Json.writeOption  "color"         x.color
-      do! Json.writeOption  "contrast"      x.contrast
-      do! Json.writeOption  "brightness"    x.brightness
-      do! Json.writeOption  "gamma"         x.gamma
-      do! Json.writeOption  "scale"         x.scale
-      do! Json.writeOption  "xRotation"     x.xRotation
-      do! Json.writeOption  "yRotation"     x.yRotation
-      do! Json.writeOption  "zRotation"     x.zRotation
+      do! Json.write              "name"          (x.name.ToString())
+      do! Json.write              "count"         x.count
+      if x.color.IsSome then
+        do! Json.writeOption      "color"       x.color
+      if x.contrast.IsSome then
+        do! Json.writeOptionFloat "contrast"    x.contrast
+      if x.brightness.IsSome then
+        do! Json.writeOptionFloat "brightness"  x.brightness
+      if x.gamma.IsSome then
+        do! Json.writeOptionFloat "gamma"       x.gamma
+      do! Json.writeOption        "scale"         x.scale
+      do! Json.writeOption        "xRotation"     x.xRotation
+      do! Json.writeOption        "yRotation"     x.yRotation
+      do! Json.writeOption        "zRotation"     x.zRotation
+      do! Json.writeOptionFloat   "maxDistance"   x.maxDistance
+      do! Json.writeOptionInt     "subsurface"    x.subsurface
+      if x.maskColor.IsSome then
+        do! Json.writeOption      "maskColor"   x.maskColor
     }
---
+++ src/PRo3D.Snapshots/Lighting-Utils.fs
@@ -0,0 +1,25 @@
+﻿namespace Aardvark.SceneGraph
+
+open Aardvark.Base
+open Adaptify
+open Aardvark.SceneGraph.Semantics
+open FSharp.Data.Adaptive
+open FShade
+    
+type LightViewProjApplicator(LightViewProj : aval<Trafo3d>, sg : ISg) =
+    inherit Sg.AbstractApplicator(sg)
+    member x.LightViewProj = LightViewProj
+    
+  
+//module ShadowSem = //TODO rno review
+//    [<Semantic>]
+//    type LightViewProjSem() = 
+//        member x.LightViewProj(r : LightViewProjApplicator) =
+//            r.Child?LightViewProj <- r.LightViewProj
+
+module ShadowSg =
+    open Aardvark.UI
+    
+    /// applies inherited attribute "LightViewProj" to a given scenegraph.
+    let applyLightViewProj (vp : aval<Trafo3d>) (sg : ISg) =
+        LightViewProjApplicator(vp,sg) |> Sg.noEvents
