21aecbd79c85f504fe44d43fbb3dfa43df144c02
Harald Steinlechner <haraldsteinlechner@gmail.com>
2020-12-23 16:41:25 +0100

    added deploy script (currently windows), version bump 3.1.1

--- src/PRo3D.Viewer/Program.fs
+++ src/PRo3D.Viewer/Program.fs
@@ -60,7 +60,7 @@ type Result =
 
 type EmbeddedRessource = EmbeddedRessource
 
-let viewerVersion       = "3.1.0"
+let viewerVersion       = "3.1.1"
 let catchDomainErrors   = false
 
 open System.IO
