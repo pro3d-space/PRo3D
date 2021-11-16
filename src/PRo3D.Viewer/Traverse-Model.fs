e68f655cae4173d84f8ed673a345b87123df532e
ThomasOrtner <ortner.t@googlemail.com>
2021-11-16 17:30:18 +0100

    startet on traverse integration

---
+++ src/PRo3D.Viewer/Traverse-Model.fs
@@ -0,0 +1,14 @@
+﻿namespace PRo3D.Viewer
+
+open Aardvark.Base
+open Adaptify
+
+[<ModelType>]
+type Traverse = 
+    {
+        positions : List<V3d>
+    }
+
+module Traverse =
+    let initial = { positions = [] }
+
---
+++ src/PRo3D.Viewer/TraverseApp.fs
@@ -0,0 +1,19 @@
+﻿namespace PRo3D.Viewer
+
+open Aardvark.Base
+open Aardvark.UI
+
+type TraverseAction =
+| LoadTraverse of string
+
+module TraverseApp =
+    let update (model : Traverse) (action : TraverseAction) : Traverse = 
+        match action with
+        | LoadTraverse path ->
+            Log.line "[Traverse] Loading %s" path
+            model
+
+    let view (model : AdaptiveTraverse) : DomNode<TraverseAction> = 
+        failwith ""
+        
+
