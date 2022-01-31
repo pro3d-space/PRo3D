52b255fc4262d6d8b4c6b8fa0f1138d3760b576b
ThomasOrtner <ortner.t@googlemail.com>
2022-01-31 17:02:21 +0100

    moved api calls to separate file API.fs

--- src/PRo3D.Lite/API.fs
+++ src/PRo3D.Lite/API.fs
@@ -1,42 +1,5 @@
 ﻿namespace PRo3D.Lite
 
-open FSharp.Data.Adaptive
-
-open Aardvark.Base
-open Aardvark.SceneGraph.Opc
-open Aardvark.Rendering
-
-open Adaptify.FSharp
-open Adaptify
-
-type Opc = {
-    opc   : PatchHierarchy
-}
-
-module PatchHierarchy = 
-    let estimateBoundingBox (p : PatchHierarchy) = 
-        match p.tree with
-        | QTree.Node(p,c) -> p.info.GlobalBoundingBox
-        | QTree.Leaf(p) -> p.info.GlobalBoundingBox
-
-[<ModelType>]
-type Surface =
-    {
-        opcs  : HashMap<string, Opc>
-        trafo : Trafo3d
-    }
-
-type Annotation = 
-    {
-        points : list<V3d>
-        color  : C4b
-    }
-
-type State = 
-    {
-        surfaces : HashMap<string, Surface>
-    }
-
 module DataAPI = 
 
     /// ray is in global coord. MarsIAU
