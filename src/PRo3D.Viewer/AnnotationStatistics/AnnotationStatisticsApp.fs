2431a655343fa0c97f70adf29dc98c7dc4d73487
DiaSc <diana_schalko@hotmail.com>
2022-03-23 17:06:30 +0100

    more histogram

--- src/PRo3D.Viewer/AnnotationStatistics/AnnotationStatisticsApp.fs
+++ src/PRo3D.Viewer/AnnotationStatistics/AnnotationStatisticsApp.fs
@@ -5,6 +5,7 @@ open PRo3D.Base
 open PRo3D.Base.Annotation
 open Aardvark.Base
 open Aardvark.UI
+open Aardvark.SceneGraph
 open PRo3D.Core
 open FSharp.Data.Adaptive
 
@@ -14,56 +15,27 @@ module AnnotationStatisticsApp =
     type AnnoStatsAction =
         | SetSelected 
 
-    let private calcMinMaxAvg (l:List<float>) =
-        match (l.IsEmpty) with
-        | true -> HashMap.empty        
-        | false -> let min = l |> List.min
-                   let max = l |> List.max
-                   let avg = l |> List.average
-                   [("min", min); ("max", max); ("avg", avg)] |> HashMap.ofList
-                   
+       
     
-
-
-
-    let calculateStats (selected:List<Annotation>) =
-
-        let lengths = selected 
-                                |> List.map(fun a -> match a.results with
-                                                            | Some r -> Some(r.length)
-                                                            | None -> None
-                                                )
-                                |> List.choose(fun o -> o)
-
-        let bearings = selected 
-                                |> List.map(fun a -> match a.results with
-                                                            | Some r -> Some(r.bearing)
-                                                            | None -> None
-                                                )
-                                |> List.choose(fun o -> o)
-
-        let lengthStats = calcMinMaxAvg lengths  
-        let bearingStats = calcMinMaxAvg bearings
-        
-        (lengthStats, bearingStats)
-    
-    
-    //recursively builds a list of bins; li = list, k = number of bins, r = rounds, acc = accumulated rangeStart, binW = bin width
-    let rec binList li k r acc binW =
+    //recursively builds a list of bins and sorts values into them
+    //li = bin list to build, sort = list of values to sort into bins; k = number of bins, r = rounds, acc = accumulated rangeStart, binW = bin width
+    let rec binList li sort k r acc binW =
         if (r > k) then (li |> IndexList.ofList)
         else 
-            let l = 
-                [
-                
-           
-                        {
-                            id = Guid.NewGuid()
-                            value = 0
-                            rangeStart = acc
-                            rangeEnd = acc + binW
-                        }
-                ]
-            binList (li @ l) k (r+1) (acc+binW) binW
+            let bin = 
+                        let rS = acc
+                        let rE = acc + binW
+                        let inBin = sort |> List.filter(fun f -> f >= rS && f <= rE)                       
+
+                        [             
+                            {
+                                id = Guid.NewGuid()
+                                value = inBin.Length
+                                rangeStart = rS
+                                rangeEnd = rE
+                            }
+                        ]
+            binList (li @ bin) sort k (r+1) (acc+binW) binW
 
         
     
@@ -78,32 +50,50 @@ module AnnotationStatisticsApp =
                     let binWidth = (max - min)/k
 
                     //set up the bins
-                    let bins = binList List.empty (int(k)) 1 min binWidth
+                    let bins = binList List.empty values (int(k)) 1 min binWidth
+                  
+                    {
+                        id = Guid.NewGuid()
+                        numOfBins = bins.Count
+                        bins = bins
+                    }                               
 
-                    //sort values into bins
-                    let valArr = values |> Array.ofList
-                    //let updatedBins = 
-                    //                    for i in 0 .. valArr.count-1
-                    //                        let len = valArr[i]
-
-                    //                        for j in 0 .. bins.count-1
-                    //                            let b = bins.TryGet j
-
-                    //                            match b with 
-                    //                             | Some bin -> 
-                    //                             | None -> 
-
-
-
-                        
-
-                    
+    
+    let private calcMinMaxAvg (l:List<float>) =
+           match (l.IsEmpty) with
+           | true -> HashMap.empty        
+           | false -> let min = l |> List.min
+                      let max = l |> List.max
+                      let avg = l |> List.average
+                      [("min", min); ("max", max); ("avg", avg)] |> HashMap.ofList
+                      
+       
 
 
 
-                    m.annoStatistics.histogram
-                    
+    let calculateStats (m: AnnoStatsModel) (selected:List<Annotation>) =
 
+           let lengths = selected 
+                                   |> List.map(fun a -> match a.results with
+                                                               | Some r -> Some(r.length)
+                                                               | None -> None
+                                                   )
+                                   |> List.choose(fun o -> o)
+
+           let bearings = selected 
+                                   |> List.map(fun a -> match a.results with
+                                                               | Some r -> Some(r.bearing)
+                                                               | None -> None
+                                                   )
+                                   |> List.choose(fun o -> o)
+
+           let lengthStats = calcMinMaxAvg lengths  
+           let bearingStats = calcMinMaxAvg bearings
+
+           //testing - histogram of lengths
+           let hist = calculateHistogram m bearings
+           
+           (lengthStats, bearingStats, hist)
 
 
     let update (m:AnnoStatsModel) (Aid:Guid) (g:GroupsModel) (a:AnnoStatsAction) =
@@ -121,20 +111,37 @@ module AnnotationStatisticsApp =
                             
                             | None -> m.selectedAnnotations   
             
-            let lens,bears = calculateStats (newList |> HashMap.toValueList)
+            let lens,bears,hist = calculateStats m (newList |> HashMap.toValueList)
             
+            
+            
+
             let stats = 
                         {
                             lengthStats = lens
                             bearingStats = bears
-                            histogram = m.annoStatistics.histogram
+                            histogram = hist
                         }
             
             {m with selectedAnnotations = newList; annoStatistics = stats}            
                          
     
-           
-    
+    let drawHistogram (h: AdaptiveHistogram) =
+
+        let attr (bin:Bin) = 
+            amap{
+                yield style "fill:white;stroke:green;stroke-width:2;fill-opacity:0.1;stroke-opacity:0.9"                
+                yield attribute "x" (sprintf "%ipx" (int(bin.rangeStart)))
+                yield attribute "y" (sprintf "%ipx" (int(bin.rangeStart + float(bin.value))))
+                yield attribute "width" (sprintf "%ipx" (int(bin.rangeEnd - bin.rangeStart)))
+                yield attribute "height" (sprintf "%ipx" (bin.value * 10))
+            } |> AttributeMap.ofAMap
+
+
+        h.bins |> AList.map (fun b -> Incremental.Svg.rect (attr b))
+
+
+        
     let view (m: AdaptiveAnnoStatsModel) =          
         
         let style' = "color: white; font-family:Consolas;"   
@@ -152,16 +159,37 @@ module AnnotationStatisticsApp =
                            let lengthStats = ("",stats.lengthStats) ||> AMap.fold(fun str (k:string) (v:float) -> sprintf "%s %s:%.2f  " str k v)
                            let bearingStats = ("",stats.bearingStats) ||> AMap.fold(fun str (k:string) (v:float) -> sprintf "%s %s:%.2f  " str k v)                              
                            
+
                            GuiEx.accordion "Statistics of selected" "Asterisk" true [
                             require GuiEx.semui (
                                Html.table [      
                                    Html.row "Length:"      [Incremental.text lengthStats] 
-                                   Html.row "Bearing:"     [Incremental.text bearingStats]                                 
+                                   Html.row "Bearing:"     [Incremental.text bearingStats]                              
                                ]
                             )
+
+                            //for testing purposes
+                            Svg.svg [style "width:200px;height:200px"] [
+                            
+                                Svg.rect (
+                                              [
+                                                 style "fill:white;stroke:green;stroke-width:2;fill-opacity:0.1;stroke-opacity:0.9"                
+                                                 attribute "x" "0"
+                                                 attribute "y" "20"
+                                                 attribute "width" "20"
+                                                 attribute "height" "20"
+                                              ]
+                                          )
+
+                            ]
+                           
                            ]
+                            
+                           
             }
-        )     
+        )
+        
+        
 
           
         
