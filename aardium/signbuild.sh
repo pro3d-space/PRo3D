49809e7213e7a9a12382b5c8a8a37b2556c45ff9
Harald Steinlechner <haraldsteinlechner@gmail.com>
2022-04-22 17:17:35 +0200

    fixed entitlements

--- aardium/signbuild.sh
+++ aardium/signbuild.sh
@@ -1,6 +1,6 @@
 find "build/build" | while read fname; do
         if [[ -f $fname ]]; then
                 echo "[INFO] Signing $fname"
-                codesign --force --timestamp --options=runtime --entitlements entitlements.mac.plist --sign "$MAC_IDENTITY" $fname
+                codesign --force --timestamp --options=runtime --entitlements ./build/entitlements.mac.plist --sign "$MAC_IDENTITY" $fname
         fi
 done
