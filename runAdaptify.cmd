d74f6f3c5077564d8e08ae33c8f065b76487856a
Harald Steinlechner <haraldsteinlechner@gmail.com>
2022-12-22 13:33:33 +0100

    removed Adaptify.MSBuild from paket.references, updated adaptify tool, switched to older dotnet in global.json, removed backup fsproj file

--- runAdaptify.cmd
+++ runAdaptify.cmd
@@ -1,1 +1,8 @@
-dotnet adaptify --local --force "C:\Users\steinlechner\Desktop\PRo3D-nomsbuild\src\PRo3D.Viewer\PRo3D.Viewer.fsproj"
+dotnet paket restore
+dotnet adaptify --local --lenses --force --addToProject src\**\*.fsproj !src\PRo3D.CorrelationPanels\PRo3D.CorrelationPanels.fsproj
+REM dotnet adaptify --local --force --addToProject src/**/*.fsproj
+REM msbuild -t:restore src\PRo3D.sln
+REM dotnet adaptify --local --force --addToProject src\PRo3D.Base\PRo3D.Base.fsproj
+REM dotnet build src\PRo3D.Base\PRo3D.Base.fsproj
+REM dotnet adaptify --local --force --addToProject src\PRo3D.Core\PRo3D.Core.fsproj
+REM dotnet build src\PRo3D.Base\PRo3D.Core.fsproj
