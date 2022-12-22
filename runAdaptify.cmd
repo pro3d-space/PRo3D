faeb106443fcfdb60ae1fbf10d9176678c28a138
Harald Steinlechner <haraldsteinlechner@gmail.com>
2022-12-22 13:38:52 +0100

    updated adaptify script

--- runAdaptify.cmd
+++ runAdaptify.cmd
@@ -1,8 +1,3 @@
+REM dotnet tool update --local adaptify --version 1.2.0-prerelease2
 dotnet paket restore
 dotnet adaptify --local --lenses --force --addToProject src\**\*.fsproj !src\PRo3D.CorrelationPanels\PRo3D.CorrelationPanels.fsproj
-REM dotnet adaptify --local --force --addToProject src/**/*.fsproj
-REM msbuild -t:restore src\PRo3D.sln
-REM dotnet adaptify --local --force --addToProject src\PRo3D.Base\PRo3D.Base.fsproj
-REM dotnet build src\PRo3D.Base\PRo3D.Base.fsproj
-REM dotnet adaptify --local --force --addToProject src\PRo3D.Core\PRo3D.Core.fsproj
-REM dotnet build src\PRo3D.Base\PRo3D.Core.fsproj
