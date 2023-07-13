2a8559e91dcf8a1f91414e83b58fbd8acda8b207
Harald Steinlechner <haraldsteinlechner@gmail.com>
2023-07-13 15:47:32 +0200

    updated packages

--- src/PRo3D.Viewer/Scene.fs
+++ src/PRo3D.Viewer/Scene.fs
@@ -110,18 +110,22 @@ module SceneLoader =
         surfaces 
         |> IndexList.map(fun s -> s, s.importPath |> getOPCxPath)
         |> IndexList.map(fun(s,p) -> 
-           match (Serialization.fileExists p) with
-           | Some path->        
+           let loadOpcX (path : string) =
                let layers = SurfaceUtils.SurfaceAttributes.read path
                let textures = layers |> SurfaceProperties.getTextures
-        
+               
                { s with 
                    scalarLayers  = layers |> SurfaceProperties.getScalarsHmap //SurfaceProperties.getScalars
                    textureLayers = textures
                    selectedTexture = textures |> IndexList.tryFirst
                }
+           match Serialization.fileExists p with
+           | Some path->        
+               loadOpcX path
            | None ->
-               s
+               match Directory.EnumerateFiles(s.importPath, "*.opcx") |> Seq.toList with
+               | [singleOpcX] -> loadOpcX singleOpcX
+               | _ -> s
         )
 
     let addGeologicSurfaces (m:Model) = 
