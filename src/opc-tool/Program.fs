f272ae1803e2eab495b76bf069c1a7b292ef0158
Harald Steinlechner <haraldsteinlechner@gmail.com>
2023-07-20 16:58:19 +0200

    creates kdtrees now

--- src/opc-tool/Program.fs
+++ src/opc-tool/Program.fs
@@ -7,18 +7,23 @@ open Aardvark.SceneGraph.Opc
 open Aardvark.GeoSpatial.Opc
 open Aardvark.VRVis.Opc
 open Aardvark.Rendering.SceneGraph.HierarchicalLoD
- 
-let serializer = FsPickler.CreateBinarySerializer()
 
-let traverse (pathHierarchies : seq<string>) : unit =
+
+let traverse (pathHierarchies: seq<string>) : unit =
     
+    let serializer = PRo3D.Base.Serialization.binarySerializer
+
     let _ =
-        pathHierarchies |> Seq.toList |> List.map (fun basePath -> 
-            let h = PatchHierarchy.load serializer.Pickle serializer.UnPickle (OpcPaths.OpcPaths basePath)
+        pathHierarchies
+        |> Seq.toList
+        |> List.map (fun basePath ->
+            let h =
+                PatchHierarchy.load serializer.Pickle serializer.UnPickle (OpcPaths.OpcPaths basePath)
             //let t = PatchLod.toRoseTree h.tree
-            let kdTrees = KdTrees.loadKdTrees' h Trafo3d.Identity true ViewerModality.XYZ serializer
-            kdTrees
-        ) 
+            let kdTrees =
+                KdTrees.loadKdTrees' h Trafo3d.Identity true ViewerModality.XYZ serializer true
+
+            kdTrees)
 
     Log.line "Done."
 
@@ -26,9 +31,13 @@ let traverse (pathHierarchies : seq<string>) : unit =
 [<EntryPoint>]
 let main args =
 
-    let hierarchies = 
-        Directory.GetDirectories(@"I:\OPC\GardenCity - Kopie") 
-        |> Seq.collect System.IO.Directory.GetDirectories
+    PRo3D.Base.Serialization.init()
+  
+    PRo3D.Base.Serialization.registry.RegisterFactory (fun _ -> KdTrees.level0KdTreePickler)
+    PRo3D.Base.Serialization.registry.RegisterFactory (fun _ -> PRo3D.Core.Surface.Init.incorePickler)
+
+    let hierarchies =
+        Directory.GetDirectories(@"F:\pro3d\data\OpcMcz")
 
     traverse hierarchies
 
