73297412c0396b7f5b3b4da9ab245cd4bb391aee
Harald Steinlechner <haraldsteinlechner@gmail.com>
2024-04-15 11:01:45 +0200

    tried to fix notarization

--- aardium/scripts/notarizeNew.js
+++ aardium/scripts/notarizeNew.js
@@ -1,11 +1,21 @@
-import { notarize } from 'electron-notarize'
+require('dotenv').config();
+const { notarize } = require('@electron-notarize');
 
-notarize({
-  appBundleId: 'space.pro3d.app',
-  appPath: `${appOutDir}/${appName}.app`,
-  appleId: "gh@aardworx.at",
-  appleIdPassword: process.env.MAC_DEV_PASSWORD,
-  ascProvider: '4LQPQ4H9LQ',
-}).catch(e => {
-  console.error("Didn't work :( " + e.message) // eslint-disable-line no-console
-})
+
+exports.default = async function(context) {
+
+	const { electronPlatformName, appOutDir } = context;
+	if (electronPlatformName !== 'darwin') {
+		return;
+	}
+ 
+	notarize({
+	  appBundleId: 'space.pro3d.app',
+	  appPath: `${appOutDir}/${appName}.app`,
+	  appleId: "gh@aardworx.at",
+	  appleIdPassword: process.env.MAC_DEV_PASSWORD,
+	  ascProvider: '4LQPQ4H9LQ',
+	}).catch(e => {
+	  console.error("Didn't work :( " + e.message) // eslint-disable-line no-console
+	});
+}   
