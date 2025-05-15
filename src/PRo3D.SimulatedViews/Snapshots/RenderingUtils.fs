f84202afed1366ae73df653d07ba9f8838f3b281
laura <laura@vrvis.at>
2025-05-15 21:35:17 +0200

    *.exr for depth panoramas

--- src/PRo3D.SimulatedViews/Snapshots/RenderingUtils.fs
+++ src/PRo3D.SimulatedViews/Snapshots/RenderingUtils.fs
@@ -6,6 +6,7 @@ open Aardvark.Rendering
 open Aardvark.SceneGraph.``Sg RuntimeCommand Extensions``
 open Aardvark.UI 
 open Aardvark.Data
+open System
 //open PixImageDevil //DevILSharp
 
 type RenderParameters =
@@ -191,15 +192,32 @@ module Rendering =
                 depthF.TryDispose () |> ignore
                 Log.line "[SNAPSHOT] Saved %s" names.exrDepth
 
-                //let loadedPi = PixImage.Load(names.exrDepth, PixImageFreeImage.Loader) |> unbox<PixImage<T>>
-                //let loadedMat = loadedPi.GetMatrix<T>()
-                //(loadedPi).Save(names.depth) 
-                //loadedMat.ForeachCoord(fun (l : V2l) -> 
-                //    let v = loadedMat.[l]
-                    
-                    //if toT l.X <> v then
-                    //    failwithf "Value mismatch at %A: expected %f, got %A" l (float32 l.X) v
-                //)
+
+                let loadedPi = PixImage.Load(names.exrDepth, PixImageFreeImage.Loader) |> unbox<PixImage<T>>
+                let loadedMat = loadedPi.GetMatrix<T>()
+                let data = loadedMat.Data.Map(fun v -> Math.Abs(v))//.IntoArray()
+                let max = Array.max data
+                let min = Array.min data
+                let max = toT 12000.0
+
+                let scaleFactor = (max - min)
+                Log.line "[SNAPSHOT] Min: %f, Max: %f, scale: %f" min max scaleFactor
+                let inline scale x =
+                    (x - min) / scaleFactor
+
+                let pi = PixImage<'t>(loadedMat.Dim, 1)
+                let depthMat = pi.GetMatrix<'t>()
+                depthMat.SetByCoord(fun (l : V2l) -> 
+                    let v = loadedMat.[l]
+                    scale (Math.Abs(v))
+                )
+
+                let saveParams = PixSaveParams(PixFileFormat.Jpeg)
+                let loader = PixImageDevil.Loader
+                let img = pi
+                loader.SaveToFile(names.depth, img, saveParams) |> ignore
+                pi.TryDispose () |> ignore
+                Log.line "[SNAPSHOT] %s" names.depth
             with e ->
                 Log.error "[SNAPSHOT] Could not save image %s" names.exrDepth
                 Log.error "%s" e.Message
