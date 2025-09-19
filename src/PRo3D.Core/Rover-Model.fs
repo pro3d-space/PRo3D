c78be91b71fb8014c6f796c7c02fdc6bb13df6a4
Martin Brunnhuber <brunn@vrvis.at>
2025-09-19 17:43:56 +0200

    Prepared Model and Shader for further debugging

--- src/PRo3D.Core/Rover-Model.fs
+++ src/PRo3D.Core/Rover-Model.fs
@@ -26,6 +26,230 @@ type RoverModel = {
 module RoverModel =
     module Shader = 
         open FShade
+        type ViewPositionAttribute() = inherit SemanticAttribute("ViewPosition")
+        type RoughnessCoordinateAttribute() = inherit SemanticAttribute("RoughnessCoordinate")
+        type MetallicnessCoordinateAttribute() = inherit SemanticAttribute("MetallicnessCoordinate")
+        type EmissiveCoordinateAttribute() = inherit SemanticAttribute("EmissiveCoordinate")
+        type NormalCoordinateAttribute() = inherit SemanticAttribute(string DefaultSemantic.NormalMapCoordinates)
+        type TangentAttribute() = inherit SemanticAttribute("Tangent")
+        type ViewTangentAttribute() = inherit SemanticAttribute("ViewTangent")
+        type ViewBiTangentAttribute() = inherit SemanticAttribute("ViewBiTangent")
+        type ViewLightDirectionAttribute() = inherit SemanticAttribute("ViewLightDirection")
+
+        let linearToSrgb (v : V4d) =
+            let e = 1.0 / 2.2
+            V4d(v.X ** e, v.Y ** e, v.Z ** e, v.W)
+
+        [<ReflectedDefinition>]
+        let srgbToLinear (v : V4d) =
+            let e = 2.2
+            V4d(v.X ** e, v.Y ** e, v.Z ** e, v.W)
+
+        let trowbridgeReitzNDF (roughness : float) (nDotH : float) =
+            let a = roughness * roughness
+            let a2 = a * a
+            let nDotH2 = nDotH * nDotH
+            let denom = nDotH2 * (a2 - 1.0) + 1.0
+            a2 / (Constant.Pi * denom * denom)
+
+        let fresnel (f0 : V3d) (nv : float) (roughness : float) =
+            let a = V3d.III * (1.0 - roughness)
+            f0 + (max f0 a - f0) * nv ** 5.0
+
+        let schlickBeckmannGAF (d : float) (roughness : float) =
+            let a = roughness * roughness
+            let k = a * 0.797884560803
+            d / (d * (1.0 - k) + k)
+                   
+
+        [<ReflectedDefinition>] [<Inline>]
+        let getR0 (reflectivity : float, metalness : float, baseColor : V3d) =
+            V3d.III * reflectivity * (1.0 - metalness) + baseColor * metalness
+
+        type UniformScope with
+            member x.DiffuseColor : V4d = uniform?Material?DiffuseColor
+            member x.Roughness : float = uniform?Material?Roughness
+            member x.Metallicness : float = uniform?Material?Metallicness
+            member x.EmissiveColor : V4d = uniform?Material?EmissiveColor
+            member x.NormalTextureScale : float = uniform?Material?NormalTextureScale
+       
+            member x.HasDiffuseColorTexture : bool = uniform?Material?HasDiffuseColorTexture
+            member x.HasRoughnessTexture : bool = uniform?Material?HasRoughnessTexture
+            member x.HasMetallicnessTexture : bool = uniform?Material?HasMetallicnessTexture
+            member x.HasEmissiveTexture : bool = uniform?Material?HasEmissiveTexture
+            member x.HasNormalTexture : bool = uniform?Material?("Has" + string DefaultSemantic.NormalMapTexture)
+       
+            member x.RoughnessTextureComponent : int = uniform?Material?RoughnessTextureComponent
+            member x.MetallicnessTextureComponent : int = uniform?Material?MetallicnessTextureComponent
+       
+            member x.HasNormals : bool = uniform?Mesh?HasNormals
+            member x.HasTangents : bool = uniform?Mesh?HasTangents
+            member x.HasColors : bool = uniform?Mesh?HasColors
+            member x.LevelCount : int = uniform?LevelCount
+       
+        type Vertex =
+            {
+                [<Position>]                pos             : V4d
+                [<ViewPosition>]            viewPos         : V4d
+                [<Normal>]                  normal          : V3d
+                [<TexCoord>]                texCoord        : V2d
+                [<RoughnessCoordinate>]     roughCoord      : V2d
+                [<MetallicnessCoordinate>]  metalCoord      : V2d
+                [<EmissiveCoordinate>]      emissCoord      : V2d
+                [<NormalCoordinate>]        normCoord       : V2d
+                [<Tangent>]                 tangent         : V4d
+                [<ViewTangent>]             viewTangent     : V3d
+                [<ViewBiTangent>]           viewBiTangent   : V3d
+                [<ViewLightDirection>]      viewLightDir    : V3d
+            }
+
+        let skyboxSpecular =
+            samplerCube {
+                texture uniform?SkyboxSpecular
+                filter Filter.MinMagMipLinear
+                addressU WrapMode.Wrap
+                addressV WrapMode.Wrap
+                addressW WrapMode.Wrap
+            }
+
+        let skyboxDiffuse =
+            samplerCube {
+                texture uniform?SkyboxDiffuse
+                filter Filter.MinMagMipLinear
+                addressU WrapMode.Wrap
+                addressV WrapMode.Wrap
+                addressW WrapMode.Wrap
+            }
+
+        let diffuseColorTex =
+            sampler2d {
+                texture uniform?DiffuseColorTexture
+                addressU WrapMode.Wrap
+                addressV WrapMode.Wrap
+                filter Filter.MinMagMipLinear
+            }
+
+        let roughnessTexture =
+            sampler2d {
+                texture uniform?RoughnessTexture
+                addressU WrapMode.Wrap
+                addressV WrapMode.Wrap
+                filter Filter.MinMagMipLinear
+            }
+
+        let metallicnessTexture =
+            sampler2d {
+                texture uniform?MetallicnessTexture
+                addressU WrapMode.Wrap
+                addressV WrapMode.Wrap
+                filter Filter.MinMagMipLinear
+            }
+
+        let emissiveTexture =
+            sampler2d {
+                texture uniform?EmissiveTexture
+                addressU WrapMode.Wrap
+                addressV WrapMode.Wrap
+                filter Filter.MinMagMipLinear
+            }
+
+        let normalTexture =
+            sampler2d {
+                texture uniform?(string DefaultSemantic.NormalMapTexture)
+                addressU WrapMode.Wrap
+                addressV WrapMode.Wrap
+                filter Filter.MinMagMipLinear
+            }
+        
+
+        let shade (v : Vertex) =
+            fragment {
+                let eps = 0.00001
+
+                let baseColor =
+                    if uniform.HasDiffuseColorTexture then
+                        let tex = diffuseColorTex.Sample(v.texCoord) |> srgbToLinear
+                        tex * uniform.DiffuseColor
+                    else
+                        uniform.DiffuseColor
+
+                let roughness =
+                    if uniform.HasRoughnessTexture then
+                        let tv = roughnessTexture.Sample(v.roughCoord).[uniform.RoughnessTextureComponent]
+                        let uv = eps + uniform.Roughness
+                        uv * tv |> saturate
+                    else
+                        uniform.Roughness + eps |> clamp 0.0 0.99
+
+                let metalness =
+                    if uniform.HasMetallicnessTexture then
+                        let tv = metallicnessTexture.Sample(v.metalCoord).[uniform.MetallicnessTextureComponent]
+                        let uv = eps + uniform.Metallicness
+                        uv * tv |> saturate
+                    else
+                        uniform.Metallicness + eps |> saturate
+
+                let occlusion = 1.0
+
+                if baseColor.W < 0.01 then discard()
+
+                let vn = v.normal |> Vec.normalize
+                let vld = v.viewLightDir |> Vec.normalize
+                let vcd = -v.viewPos.XYZ |> Vec.normalize
+
+                let half =
+                    let v = vld + vcd
+                    let l = Vec.Length v
+                    if l > eps then v / l
+                    else V3d.Zero
+
+                let vn =
+                    if uniform.HasNormalTexture then
+                        let vt = Vec.normalize v.viewTangent
+                        let vb = Vec.normalize v.viewBiTangent
+
+                        let v = normalTexture.Sample(v.normCoord).XYZ
+                        let nn = (v * 2.0 - 1.0) * V3d(V2d.II * uniform.NormalTextureScale * 0.5, 1.0)
+
+                        let newNormal = vn * nn.Z + vt * nn.X + vb * nn.Y |> Vec.normalize
+                        if newNormal.Z < 0.0 then vn
+                        else newNormal
+                    else
+                        vn
+
+
+                let refl = -Vec.reflect vn vcd
+
+                let nl = Vec.dot vn vld |> max 0.0
+                let nh = Vec.dot vn half |> max 0.0
+                //let hv = Vec.dot half vcd |> max 0.0
+                let nv = Vec.dot vn vcd |> max 0.0
+
+
+
+                let f0 = getR0(0.04, metalness, V3d.III) * baseColor.XYZ
+                let d = trowbridgeReitzNDF nh roughness
+
+                let f = fresnel f0 nv roughness
+                let g = schlickBeckmannGAF nv roughness * schlickBeckmannGAF nl roughness
+
+                let lambert = nl * 3.0
+                let dr = V3d.III * occlusion
+
+                let diffuseDirectTerm = (baseColor.XYZ / Constant.Pi) * (V3d.III - f) * (1.0 - metalness)
+
+                let specularDirectTerm =
+                    (f * g * d) / (4.0 * nl * nv + eps)
+
+                let brdfDirectOutput = (diffuseDirectTerm + specularDirectTerm) * lambert * dr
+                let ambientDiffuse = (baseColor.XYZ / Constant.Pi) * (1.0 - f) * (1.0 - metalness)
+
+                let ambientSpecular = f
+
+                let color = brdfDirectOutput + ambientDiffuse + ambientSpecular
+
+                return V4d(saturate color, 1.0) |> linearToSrgb
+            }
 
 
     let initial = {
@@ -79,7 +303,7 @@ module RoverModel =
                     for sem in sems do
                         let semantic =
                             match sem with
-                            | TextureSemantic.BaseColor -> DefaultSemantic.DiffuseColorCoordinates
+                            | TextureSemantic.BaseColor -> DefaultSemantic.DiffuseColorTexture
                             | TextureSemantic.Roughness -> Semantic.RoughnessCoordinate
                             | TextureSemantic.Emissive -> Semantic.EmissiveCoordinate
                             | TextureSemantic.Metallicness -> Semantic.MetallicnessCoordinate
@@ -224,7 +448,7 @@ module RoverModel =
                 |> SgFSharp.Sg.vertexBufferValue' DefaultSemantic.Colors C4b.White
                 |> SgFSharp.Sg.vertexBufferValue' DefaultSemantic.Normals V3f.OOI
                 |> SgFSharp.Sg.vertexBufferValue' Semantic.Tangent V4f.IOOI
-                |> SgFSharp.Sg.vertexBufferValue' DefaultSemantic.DiffuseColorCoordinates V2f.Zero
+                |> SgFSharp.Sg.vertexBufferValue' DefaultSemantic.DiffuseColorTexture V2f.Zero
                 |> SgFSharp.Sg.vertexBufferValue' Semantic.RoughnessCoordinate V2f.Zero
                 |> SgFSharp.Sg.vertexBufferValue' Semantic.EmissiveCoordinate V2f.Zero
                 |> SgFSharp.Sg.vertexBufferValue' Semantic.MetallicnessCoordinate V2f.Zero
@@ -241,6 +465,10 @@ module RoverModel =
                 toEffect PRo3D.Base.Shader.DepthOffset.depthOffsetFS
             ]
             |> Sg.onOff visible
+            |> Sg.shader {
+                //do! Shader.trafo
+                do! Shader.shade
+            }
 
         
 
