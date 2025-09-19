97cc3f12588f81cbf259eb51765b0ced81b0f0cb
Martin Brunnhuber <brunn@vrvis.at>
2025-09-19 14:07:59 +0200

    First working Example of Rover Import with different model.

--- src/PRo3D.Viewer/TraverseApp.fs
+++ src/PRo3D.Viewer/TraverseApp.fs
@@ -15,6 +15,7 @@ open Aardvark.Rendering
 open FSharp.Data.Adaptive
 open FSharp.Data.Adaptive.Operators
 open Aardvark.UI.Trafos
+open Aardvark.Data.GLTF
         
 module Double =
     let parse x =
@@ -381,7 +382,7 @@ module TraverseApp =
             let colors = ColorBrewer.twelveClassPaired |> List.map ColorBrewer.toMaxValue
             traverses |> ColorBrewer.assignColors colors
         else    
-            traverses |> List.map(fun x -> (x, C4b.White))
+            traverses |> List.map(fun x -> (x, C4b.Chocolate))
 
     let calculateRoverPosition (model : TraverseModel) (newPosAction : Numeric.Action)=
         let roverLocation = 
@@ -668,13 +669,205 @@ module TraverseApp =
             |> ASet.map snd 
             |> Sg.set
 
+        module Semantic =
+            let RoughnessCoordinate = Symbol.Create "RoughnessCoordinate"
+            let MetallicnessCoordinate = Symbol.Create "MetallicnessCoordinate"
+            let EmissiveCoordinate = Symbol.Create "EmissiveCoordinate"
+            let NormalCoordinate = DefaultSemantic.NormalMapCoordinates
+            let Tangent = Symbol.Create "Tangent"
+
+        let loadRoverModel () = 
+            let roverFile = @"D:\Temp\RoverTesting\Export_YoqSothoth\Perseverance_100.gltf"
+
+            let model = GLTF.tryLoad roverFile
+
+            let meshSg (m : Mesh) =
+                let fvc =
+                    match m.Index with
+                    | Some i -> i.Length
+                    | None -> m.Positions.Length
+
+                let mutable sg =
+                    Sg.render m.Mode (DrawCallInfo(FaceVertexCount = fvc, InstanceCount = 1))
+                    |> Sg.vertexAttribute' DefaultSemantic.Positions m.Positions
+
+                match m.Index with
+                | Some idx -> sg <- sg |> Sg.indexArray idx
+                | None -> ()
+
+                match m.Normals with
+                | Some ns -> sg <- sg |> Sg.vertexAttribute' DefaultSemantic.Normals ns
+                | None -> ()
+
+                match m.Tangents with
+                | Some ns -> sg <- sg |> Sg.vertexAttribute' Semantic.Tangent ns
+                | None -> ()
+
+                match m.Colors with
+                | Some cs -> sg <- sg |> Sg.vertexAttribute' DefaultSemantic.Colors cs
+                | None -> sg <- sg |> Sg.vertexAttribute' DefaultSemantic.Colors (Array.create m.Positions.Length C4b.White)
+
+                for data, sems in m.TexCoords do
+                    let view = BufferView.ofArray data
+                    for sem in sems do
+                        let semantic =
+                            match sem with
+                            | TextureSemantic.BaseColor -> DefaultSemantic.DiffuseColorCoordinates
+                            | TextureSemantic.Roughness -> Semantic.RoughnessCoordinate
+                            | TextureSemantic.Emissive -> Semantic.EmissiveCoordinate
+                            | TextureSemantic.Metallicness -> Semantic.MetallicnessCoordinate
+                            | TextureSemantic.Normal -> Semantic.NormalCoordinate
+
+                        sg <- sg |> Sg.vertexBuffer semantic view
+
+                let uniforms =
+                    UniformProvider.ofList [
+                        "HasNormals", AVal.constant (Option.isSome m.Normals) :> IAdaptiveValue
+                        "HasTangents", AVal.constant (Option.isSome m.Tangents)
+                        "HasColors", AVal.constant (Option.isSome m.Colors)
+                    ]
+
+                Sg.UniformApplicator(uniforms, sg) :> ISg
+
+            model 
+            |> Option.map (fun scene -> 
+
+                let textures =
+                        scene.ImageData |> Map.map (fun _ data ->
+                            let texture = StreamTexture(fun () -> new MemoryStream(data.Data))
+                            texture :> ITexture |> AVal.constant
+                        )
+
+                let meshes =
+                    scene.Meshes |> Map.map (fun _ m -> meshSg m)
+
+                let rec traverse (node : Node) : ISg =
+                    let cs =
+                        match node.Children with
+                        | [] -> None
+                        | _ -> node.Children |> Seq.map traverse |> SgFSharp.Sg.ofSeq |> Some
+
+                    let ms =
+                        node.Meshes |> List.choose (fun mi ->
+                            match Map.tryFind mi.Mesh meshes with
+                            | Some mesh ->
+                                match mi.Material |> Option.bind (fun mid -> Map.tryFind mid scene.Materials) with
+                                | Some mat ->
+                                    let uniforms =
+                                        let baseColorTexture =
+                                            mat.BaseColorTexture 
+                                            |> Option.bind (fun id -> Map.tryFind id textures)
+                                            |> Option.defaultValue (NullTexture.Instance |> AVal.constant)
+                                            
+                                        let roughnessTexture =
+                                            mat.RoughnessTexture 
+                                            |> Option.bind (fun id -> Map.tryFind id textures)
+                                            |> Option.defaultValue (NullTexture.Instance |> AVal.constant)
+                                            
+                                        let metallicnessTexture =
+                                            mat.MetallicnessTexture 
+                                            |> Option.bind (fun id -> Map.tryFind id textures)
+                                            |> Option.defaultValue (NullTexture.Instance |> AVal.constant)
+
+                                        let normalTexture =
+                                            mat.NormalTexture 
+                                            |> Option.bind (fun id -> Map.tryFind id textures)
+                                            |> Option.defaultValue (NullTexture.Instance |> AVal.constant)
+
+                                        let emissiveTexture =
+                                            mat.EmissiveTexture 
+                                            |> Option.bind (fun id -> Map.tryFind id textures)
+                                            |> Option.defaultValue (NullTexture.Instance |> AVal.constant)
+                                            
+                                        UniformProvider.ofList [
+                                            "DiffuseColor", AVal.constant mat.BaseColor :> IAdaptiveValue
+                                            "Roughness", AVal.constant mat.Roughness
+                                            "Metallicness", AVal.constant mat.Metallicness
+                                            "EmissiveColor", AVal.constant mat.EmissiveColor
+                                            "NormalTextureScale", AVal.constant mat.NormalTextureScale
+
+                                            "HasDiffuseColorTexture", AVal.constant (Option.isSome mat.BaseColorTexture)
+                                            "HasRoughnessTexture", AVal.constant (Option.isSome mat.RoughnessTexture)
+                                            "HasMetallicnessTexture", AVal.constant (Option.isSome mat.MetallicnessTexture)
+                                            "HasEmissiveTexture", AVal.constant (Option.isSome mat.EmissiveTexture)
+                                            "Has" + string DefaultSemantic.NormalMapTexture, AVal.constant (Option.isSome mat.NormalTexture)
+
+                                            "DiffuseColorTexture", baseColorTexture
+                                            "RoughnessTexture", roughnessTexture
+                                            "MetallicnessTexture", metallicnessTexture
+                                            "RoughnessTextureComponent", AVal.constant mat.RoughnessTextureComponent
+                                            "MetallicnessTextureComponent", AVal.constant mat.MetallicnessTextureComponent
+                                            string DefaultSemantic.NormalMapTexture, normalTexture
+                                            "EmissiveTexture", emissiveTexture
+                                        ]
+                                    Some (Sg.UniformApplicator(uniforms, mesh) :> ISg)
+                                | None ->
+                                    Some mesh
+                            | None ->
+                                None
+                        )                         
+                        |> SgFSharp.Sg.ofList |> Some
+
+                    let sg =
+                        match cs with
+                        | Some cs ->
+                            ms 
+                            |> Option.map(fun m -> SgFSharp.Sg.ofList [cs; m])
+                            |> Option.defaultValue cs
+                        | None ->
+                            ms
+                            |> Option.map(fun m -> m)
+                            |> Option.defaultValue SgFSharp.Sg.empty
+                            
+                    node.Trafo
+                    |> Option.map (fun t -> SgFSharp.Sg.trafo' t sg)
+                    |> Option.defaultValue sg
+
+                traverse scene.RootNode)
+            |> Option.defaultValue SgFSharp.Sg.empty
+            
+        let defaultMaterial =
+            UniformProvider.ofList [
+                "DiffuseColor", AVal.constant C4f.White :> IAdaptiveValue
+                "Roughness", AVal.constant 0.0
+                "Metallicness", AVal.constant 0.0
+                "EmissiveColor", AVal.constant C4f.Black
+                "NormalTextureScale", AVal.constant 1.0
+
+                "HasDiffuseColorTexture", AVal.constant false
+                "HasRoughnessTexture", AVal.constant false
+                "HasMetallicnessTexture", AVal.constant false
+                "HasEmissiveTexture", AVal.constant false
+                "Has" + string DefaultSemantic.NormalMapTexture, AVal.constant false
+
+                "DiffuseColorTexture", AVal.constant NullTexture.Instance
+                "RoughnessTexture", AVal.constant NullTexture.Instance
+                "RoughnessTextureComponent", AVal.constant 0
+                "MetallicnessTexture", AVal.constant NullTexture.Instance
+                "MetallicnessTextureComponent", AVal.constant 0
+                string DefaultSemantic.NormalMapTexture, AVal.constant NullTexture.Instance
+                "EmissiveTexture", AVal.constant NullTexture.Instance
+            ]
+
         let viewRover (refSystem : AdaptiveReferenceSystem) (traverseModel : AdaptiveTraverseModel) = 
             let visible = 
                 traverseModel.roverTraverse |> AVal.map(fun rT -> rT.IsSome)
             
-            let roverModel = Sg.box (AVal.constant(C4b.Red)) (AVal.constant(Box3d.Unit))
+            let roverModel = loadRoverModel()//Sg.box (AVal.constant(C4b.Red)) (AVal.constant(Box3d.Unit))
             
-            roverModel
+            let rM = 
+                Sg.UniformApplicator(defaultMaterial, roverModel)
+                |> SgFSharp.Sg.vertexBufferValue' DefaultSemantic.Colors C4b.White
+                |> SgFSharp.Sg.vertexBufferValue' DefaultSemantic.Normals V3f.OOI
+                |> SgFSharp.Sg.vertexBufferValue' Semantic.Tangent V4f.IOOI
+                |> SgFSharp.Sg.vertexBufferValue' DefaultSemantic.DiffuseColorCoordinates V2f.Zero
+                |> SgFSharp.Sg.vertexBufferValue' Semantic.RoughnessCoordinate V2f.Zero
+                |> SgFSharp.Sg.vertexBufferValue' Semantic.EmissiveCoordinate V2f.Zero
+                |> SgFSharp.Sg.vertexBufferValue' Semantic.MetallicnessCoordinate V2f.Zero
+                |> SgFSharp.Sg.vertexBufferValue' Semantic.NormalCoordinate V2f.Zero
+                |> Sg.noEvents
+
+            rM
             |> Sg.translation traverseModel.roverLocation
             |> Sg.uniform "DepthOffset" (AVal.constant 0.0000000001)
             |> Sg.blendMode (AVal.constant BlendMode.None)
