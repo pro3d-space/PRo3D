cbe5c1fb0150bf0c620e36b8812f011e78efcf90
Harald Steinlechner <haraldsteinlechner@gmail.com>
2025-12-16 17:48:36 +0100

    added annoation export test case & anno loading

--- src/Tests/GeoJsonExportTest.fs
+++ src/Tests/GeoJsonExportTest.fs
@@ -1,79 +1,77 @@
 ﻿namespace GeoJsonRework
 
-module GeoJsonExportTest =
+
+open Aardvark.Base
+open PRo3D.Core
+open PRo3D.Base.Annotation
+open FSharp.Data.Adaptive
+open Adaptify.FSharp
+open PRo3D.Core.Drawing
 
 
-    open Aardvark.Base
-    open PRo3D.Core
-    open PRo3D.Base.Annotation
-    open FSharp.Data.Adaptive
-    open Adaptify.FSharp
 
+module GeoJsonImportExport =  
 
-    module GeoJsonImportExport =  
+    // requirements
+    // - geojson conformant export
+    // - should be compatible with qgis and arcgis
+    // - additional properties: isSelected, ellipse
+    // - support for cartesian and geographic coordinates
+    //    - by default export in geographic coordinates (lon, lat, alt)
+    //    - flag while export, whether cartesian coordiantes shall be included (in properties field)
+    // - if cartesian coordinates are included, the reference frame must be included as well (in properties field top level meta data)
+    // - support for different annotation types (point, line, polygon, ellipse, ...)
 
-        // requirements
-        // - geojson conformant export
-        // - should be compatible with qgis and arcgis
-        // - additional properties: isSelected, ellipse
-        // - support for cartesian and geographic coordinates
-        //    - by default export in geographic coordinates (lon, lat, alt)
-        //    - flag while export, whether cartesian coordiantes shall be included (in properties field)
-        // - if cartesian coordinates are included, the reference frame must be included as well (in properties field top level meta data)
-        // - support for different annotation types (point, line, polygon, ellipse, ...)
-
-        (*
+    (*
     
-       {
-           "type": "FeatureCollection",
-           "features": [{
-	           "type": "Feature",
-	           "geometry": {
-		           "type": "Point",
-		           "coordinates": [102.0, 0.5],
-                   "properties": {
-                        "cartesianCoordinates": [x, y, z]
-                    }
-	           },
-	           "properties": {
-		           "referenceFrame": "IAU_MARS" // only if cartesian coordinates are included
-	           }
-             }]
-       }
+    {
+        "type": "FeatureCollection",
+        "features": [{
+	        "type": "Feature",
+	        "geometry": {
+		        "type": "Point",
+		        "coordinates": [102.0, 0.5],
+                "properties": {
+                    "cartesianCoordinates": [x, y, z]
+                }
+	        },
+	        "properties": {
+		        "referenceFrame": "IAU_MARS" // only if cartesian coordinates are included
+	        }
+            }]
+    }
     
-        *)
+    *)
 
-        (*
+    (*
     
-        earlier: Annotations -> GeometryCollection -> Chiron Serilizer 
-        then: Annoations -> string (via json lib)
+    earlier: Annotations -> GeometryCollection -> Chiron Serilizer 
+    then: Annoations -> string (via json lib)
 
-        read in: string -> Annoations (low prio)
+    read in: string -> Annoations (low prio)
 
-        *)
+    *)
 
 
-        let test (a : Annotation) = 
-            ()
+    type CoordinateConfiguration =
+        | CartesianOnly of targetReferenceFrame : string
+        | GeographicOnly
+        | Both of targetReferenceFrame : string
 
-        type CoordinateConfiguration =
-            | CartesianOnly of referenceFrame : string
-            | GeographicOnly
-            | Both of referenceFrame : string
-
-        type LatLonAlt = V3d
-        type Cartesian = V3d
+    type LatLonAlt = V3d
+    type Cartesian = V3d
+    type ReferenceFrame = string
+    type Body = string
     
 
-        let toJsonString (referenceFrame : string) 
-                         (configuration : CoordinateConfiguration) 
-                         (toLatLonAlt : Cartesian -> LatLonAlt)
-                         (a : Annotations) : string = 
-            failwith ""
+    let toJsonString (configuration : CoordinateConfiguration) 
+                     (toLatLonAlt :   Body -> ReferenceFrame ->  V3d -> Option<LatLonAlt>) // deal with case transformation does not work
+                     (a : Annotations) : string = 
+        failwith ""
 
 
-        let annoations = 
-            { Annotation.initial with geometry = Geometry.Point; points = IndexList.ofList [V3d(1.0,10.0,10.0)] }
+    let annoations = 
+        { Annotation.initial with geometry = Geometry.Point; points = IndexList.ofList [V3d(1.0,10.0,10.0)] }
 
 
 module Tests =
@@ -83,12 +81,10 @@ module Tests =
 
     open Expecto
 
-    open FSharp.NativeInterop
-
-    open Aardvark.Base
-
     open PRo3D.Extensions
     open PRo3D.Extensions.FSharp
+
+    open GeoJsonImportExport
 
     let logDir = Path.Combine(".", "logs")
 
@@ -107,9 +103,10 @@ module Tests =
         else
             Some(V3d(x, y, z))
 
-    let xyz2LatLonAlt (body : string) (x : float) (y : float) (z : float) =
+    let xyz2LatLonAlt (body : string) (referenceFrame : string) (cartesian : V3d)=
         let mutable lat, lon, alt = 0.0, 0.0, 0.0
-        let res = CooTransformation.Xyz2LatLonAlt(body, x, y, z, &lat, &lon, &alt)
+        Log.warn "reference frame + body  not yet done, assuming IAU_MARS"
+        let res = CooTransformation.Xyz2LatLonAlt(body, cartesian.X,cartesian.Y, cartesian.Z, &lat, &lon, &alt)
         if res <> 0 then
             None
         else
@@ -131,7 +128,14 @@ module Tests =
 
             test "GetLatLon" {
                 let pos = V3d(693177.2106401927, -3147511.6703961412, 1070879.1507304527)
-                let latlon = xyz2LatLonAlt "MARS" pos.X pos.Y pos.Z
+                let latlon = xyz2LatLonAlt "MARS" "IAU_MARS" pos
                 Expect.isSome latlon "could not get lat lon"
+            }
+
+            test "SerializeMarsEllipses" {
+                let annotationPath = Path.combine [__SOURCE_DIRECTORY__; "data"; "mola-annotations.pro3d.ann"]
+                let annotations = DrawingUtilities.IO.loadAnnotationsFromFile annotationPath
+                let t = GeoJsonImportExport.toJsonString (Both "IAU_MARS") xyz2LatLonAlt annotations
+                Expect.isNotEmpty t "could not serialize annotations"
             }
         ]
