c1f2bf9fdc25e62083618f5190de492b40ac421f
sudokuMonaco <sophie.pichler@ufficina.de>
2026-01-09 12:38:04 +0100

    Move geojsonModule to PRo3d.Base

---
+++ src/PRo3D.Base/Annotation/Exporters/GeoJSON.Export.Thoth.fs
@@ -0,0 +1,108 @@
+﻿namespace PRo3D.Base.Annotation
+
+open PRo3D.Core
+open PRo3D.Base.Annotation
+open Thoth.Json.Net
+
+open Aardvark.Base
+open FSharp.Data.Adaptive
+
+module GeoJsonThoth =
+
+    let coordinate (p : V3d) =
+        Encode.list [ Encode.float p.X; Encode.float p.Y; Encode.float p.Z ]
+
+    let featureProperties (annotation: Annotation) =
+        match annotation.geometry with
+        | Geometry.AxisEllipse ->
+            Encode.object [
+                "ellipse", Encode.bool true
+            ]
+        | _ -> 
+            Encode.object [
+            ]
+
+    let featureGeometry (annotation: Annotation) =
+        match annotation.geometry with
+        | Geometry.Point ->
+            let p =
+                annotation.points
+                |> Seq.head
+            Encode.object [
+                "type", Encode.string "Point"
+                "coordinates", coordinate p
+                "properties", Encode.object []
+            ]
+        | Geometry.Line
+        | Geometry.Polyline
+        | Geometry.DnS
+        | Geometry.TT ->
+            let coordinates =
+                annotation.points
+                |> Seq.map coordinate
+                |> Seq.toList
+            Encode.object [
+                "type", Encode.string "LineString"
+                "coordinates", Encode.list coordinates
+                "properties", Encode.object []
+            ]
+        | Geometry.Polygon ->
+            let coordinates =
+                annotation.points
+                |> Seq.map coordinate
+                |> Seq.toList
+            Encode.object [
+                "type", Encode.string "Polygon"
+                "coordinates", Encode.list [ Encode.list coordinates ]
+                "properties", Encode.object []
+            ]
+        | Geometry.AxisEllipse ->
+            let coordinates =
+                annotation.points
+                |> Seq.map coordinate
+                |> Seq.toList
+            let coordinatesClosed =
+                match coordinates with
+                | [] -> []
+                | h :: _ -> coordinates @ [h]
+            Encode.object [
+                "type", Encode.string "Polygon"
+                "coordinates", Encode.list [ Encode.list coordinatesClosed ]
+                "properties", Encode.object []
+            ]
+        | _ ->
+            Encode.object [
+                "type", Encode.string "unknown"
+                "coordinates", Encode.list []
+            ]
+
+
+    let feature (annotation : Annotation) =
+        Encode.object [
+            "type", Encode.string "Feature"
+            "geometry", featureGeometry annotation
+            "properties", featureProperties annotation
+        ]
+
+    let globalProperties (annotations : Annotation list) =
+        Encode.object [
+            "referenceFrame", Encode.string "IAU_MARS" // TODO
+        ]
+
+    let featureCollection (annotations : Annotation list) (encodeCartesian : bool)=
+        match encodeCartesian with
+        | true ->
+            Encode.object [
+                "type", Encode.string "FeatureCollection"
+                "features", Encode.list (annotations |> List.map (fun ann -> feature ann))
+                "properties", globalProperties annotations
+
+            ]
+        | false ->
+            Encode.object [
+                "type", Encode.string "FeatureCollection"
+                "features", Encode.list (annotations |> List.map (fun ann -> feature ann))
+            ]
+
+    let encoder (annotations : Annotation list) : JsonValue =
+        featureCollection annotations true
--- src/Tests/GeoJsonExportTest.fs
+++ src/Tests/GeoJsonExportTest.fs
@@ -1,112 +1,9 @@
 ﻿namespace GeoJsonRework
 
-open Thoth.Json.Net
-
 open Aardvark.Base
 open PRo3D.Core
 open PRo3D.Base.Annotation
 open FSharp.Data.Adaptive
-
-module GeoJson =
-
-    let coordinate (p : V3d) =
-        Encode.list [ Encode.float p.X; Encode.float p.Y; Encode.float p.Z ]
-
-    let featureProperties (annotation: Annotation) =
-        match annotation.geometry with
-        | Geometry.AxisEllipse ->
-            Encode.object [
-                "ellipse", Encode.bool true
-            ]
-        | _ -> 
-            Encode.object [
-            ]
-
-    let featureGeometry (annotation: Annotation) =
-        match annotation.geometry with
-        | Geometry.Point ->
-            let p =
-                annotation.points
-                |> Seq.head
-            Encode.object [
-                "type", Encode.string "Point"
-                "coordinates", coordinate p
-                "properties", Encode.object []
-            ]
-        | Geometry.Line
-        | Geometry.Polyline
-        | Geometry.DnS
-        | Geometry.TT ->
-            let coordinates =
-                annotation.points
-                |> Seq.map coordinate
-                |> Seq.toList
-            Encode.object [
-                "type", Encode.string "LineString"
-                "coordinates", Encode.list coordinates
-                "properties", Encode.object []
-            ]
-        | Geometry.Polygon ->
-            let coordinates =
-                annotation.points
-                |> Seq.map coordinate
-                |> Seq.toList
-            Encode.object [
-                "type", Encode.string "Polygon"
-                "coordinates", Encode.list [ Encode.list coordinates ]
-                "properties", Encode.object []
-            ]
-        | Geometry.AxisEllipse ->
-            let coordinates =
-                annotation.points
-                |> Seq.map coordinate
-                |> Seq.toList
-            let coordinatesClosed =
-                match coordinates with
-                | [] -> []
-                | h :: _ -> coordinates @ [h]
-            Encode.object [
-                "type", Encode.string "Polygon"
-                "coordinates", Encode.list [ Encode.list coordinatesClosed ]
-                "properties", Encode.object []
-            ]
-        | _ ->
-            Encode.object [
-                "type", Encode.string "unknown"
-                "coordinates", Encode.list []
-            ]
-
-
-    let feature (annotation : Annotation) =
-        Encode.object [
-            "type", Encode.string "Feature"
-            "geometry", featureGeometry annotation
-            "properties", featureProperties annotation
-        ]
-
-    let globalProperties annotations =
-        Encode.object [
-            "referenceFrame", Encode.string "IAU_MARS" // TODO
-        ]
-
-    let featureCollection (annotations : Annotations) (encodeCartesian : bool)=
-        let flattedAnnotations = annotations.annotations.flat |> HashMap.toList |> List.map snd |> List.map Leaf.toAnnotation
-        match encodeCartesian with
-        | true ->
-            Encode.object [
-                "type", Encode.string "FeatureCollection"
-                "features", Encode.list (flattedAnnotations |> List.map (fun ann -> feature ann))
-                "properties", globalProperties annotations
-
-            ]
-        | false ->
-            Encode.object [
-                "type", Encode.string "FeatureCollection"
-                "features", Encode.list (flattedAnnotations |> List.map (fun ann -> feature ann))
-            ]
-
-    let encoder (annotations : Annotations) : JsonValue =
-        featureCollection annotations true
 
 module GeoJsonExportTest =
 
@@ -242,8 +139,9 @@ module Tests =
             }
 
             test "SerializeIncome" {
-                let annotation = readJson("annotation_3.ann")
-                let parsedAnnotation = GeoJson.encoder annotation
+                let annotations = readJson("annotation_3.ann")
+                let flattedAnnotations = annotations.annotations.flat |> HashMap.toList |> List.map snd |> List.map Leaf.toAnnotation
+                let parsedAnnotation = GeoJsonThoth.encoder flattedAnnotations
                 Expect.isTrue parsedAnnotation.HasValues "..."            }
 
             (*
