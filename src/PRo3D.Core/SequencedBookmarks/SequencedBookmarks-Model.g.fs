a5bdb775e2c1f0c2c4b531f06cedef272dbba213
Sophie Pichler <pichler@VRVIS.NET>
2026-07-15 17:31:16 +0200

    Merge stage issues....

--- src/PRo3D.Core/SequencedBookmarks/SequencedBookmarks-Model.g.fs
+++ src/PRo3D.Core/SequencedBookmarks/SequencedBookmarks-Model.g.fs
@@ -1,5 +1,9 @@
 //e4eebad7-e3e9-5330-2090-496649038ecb
+<<<<<<< Updated upstream
 //ab5c4ff4-9b32-a3bb-7d38-d8d1a5b901a7
+=======
+//2ba321cf-6d65-80fe-e10c-e7a1c5e846a3
+>>>>>>> Stashed changes
 #nowarn "49" // upper case patterns
 #nowarn "66" // upcast is unncecessary
 #nowarn "1337" // internal types
@@ -12,11 +16,19 @@ open Adaptify
 open PRo3D.Core.SequencedBookmarks
 [<System.Diagnostics.CodeAnalysis.SuppressMessage("NameConventions", "*")>]
 type AdaptiveSequencedBookmarkModel(value : SequencedBookmarkModel) =
+<<<<<<< Updated upstream
     let mutable _path_ = FSharp.Data.Adaptive.cval(value.path)
     let mutable _cameraView_ = FSharp.Data.Adaptive.cval(value.cameraView)
     let mutable _filename_ = FSharp.Data.Adaptive.cval(value.filename)
     let mutable _name_ = FSharp.Data.Adaptive.cval(value.name)
     let mutable _key_ = FSharp.Data.Adaptive.cval(value.key)
+=======
+    let mutable _filename_ = FSharp.Data.Adaptive.cval(value.filename)
+    let mutable _cameraView_ = FSharp.Data.Adaptive.cval(value.cameraView)
+    let mutable _name_ = FSharp.Data.Adaptive.cval(value.name)
+    let mutable _key_ = FSharp.Data.Adaptive.cval(value.key)
+    let mutable _path_ = FSharp.Data.Adaptive.cval(value.path)
+>>>>>>> Stashed changes
     let _bookmark_ = PRo3D.Core.AdaptiveBookmark(value.bookmark)
     let _metadata_ = FSharp.Data.Adaptive.cval(value.metadata)
     let _frustumParameters_ = FSharp.Data.Adaptive.cval(value.frustumParameters)
@@ -41,11 +53,19 @@ type AdaptiveSequencedBookmarkModel(value : SequencedBookmarkModel) =
         if Microsoft.FSharp.Core.Operators.not((FSharp.Data.Adaptive.ShallowEqualityComparer<SequencedBookmarkModel>.ShallowEquals(value, __value))) then
             __value <- value
             __adaptive.MarkOutdated()
+<<<<<<< Updated upstream
             _path_.Value <- value.path
             _cameraView_.Value <- value.cameraView
             _filename_.Value <- value.filename
             _name_.Value <- value.name
             _key_.Value <- value.key
+=======
+            _filename_.Value <- value.filename
+            _cameraView_.Value <- value.cameraView
+            _name_.Value <- value.name
+            _key_.Value <- value.key
+            _path_.Value <- value.path
+>>>>>>> Stashed changes
             _bookmark_.Update(value.bookmark)
             _metadata_.Value <- value.metadata
             _frustumParameters_.Value <- value.frustumParameters
@@ -56,11 +76,19 @@ type AdaptiveSequencedBookmarkModel(value : SequencedBookmarkModel) =
             _duration_.Update(value.duration)
             _observationInfo_.Update(value.observationInfo)
     member __.Current = __adaptive
+<<<<<<< Updated upstream
     member __.path = _path_ :> FSharp.Data.Adaptive.aval<Microsoft.FSharp.Core.string>
     member __.cameraView = _cameraView_ :> FSharp.Data.Adaptive.aval<Aardvark.Rendering.CameraView>
     member __.filename = _filename_ :> FSharp.Data.Adaptive.aval<Microsoft.FSharp.Core.string>
     member __.name = _name_ :> FSharp.Data.Adaptive.aval<Microsoft.FSharp.Core.string>
     member __.key = _key_ :> FSharp.Data.Adaptive.aval<System.Guid>
+=======
+    member __.filename = _filename_ :> FSharp.Data.Adaptive.aval<Microsoft.FSharp.Core.string>
+    member __.cameraView = _cameraView_ :> FSharp.Data.Adaptive.aval<Aardvark.Rendering.CameraView>
+    member __.name = _name_ :> FSharp.Data.Adaptive.aval<Microsoft.FSharp.Core.string>
+    member __.key = _key_ :> FSharp.Data.Adaptive.aval<System.Guid>
+    member __.path = _path_ :> FSharp.Data.Adaptive.aval<Microsoft.FSharp.Core.string>
+>>>>>>> Stashed changes
     member __.version = __value.version
     member __.bookmark = _bookmark_
     member __.metadata = _metadata_ :> FSharp.Data.Adaptive.aval<Microsoft.FSharp.Core.option<Microsoft.FSharp.Core.string>>
